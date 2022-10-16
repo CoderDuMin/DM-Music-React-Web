c39c27f0b3da1f3e6654fd4d795e477b6fa62e02
CoderDuMin <1325964835@qq.com>
2022-10-16 22:39:07 +0800

    配置相关

--- src/service/config.js
+++ src/service/config.js
@@ -1,5 +1,5 @@
 const devBaseURL = "http://1.117.247.44:5502";
-const proBaseURL = "https://production.org";
+const proBaseURL = "http://1.117.247.44:5502";
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL: proBaseURL;
 
 export const TIMEOUT = 10000;
