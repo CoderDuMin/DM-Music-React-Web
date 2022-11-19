7a5605716990599d1c4a3cecb5f3640c993c49cc
CoderDuMin <1325964835@qq.com>
2022-11-19 23:57:34 +0800

    service配置变更

--- src/service/config.js
+++ src/service/config.js
@@ -1,5 +1,5 @@
-const devBaseURL = "http://1.117.247.44:5502";
-const proBaseURL = "http://1.117.247.44:5502";
+const devBaseURL = "http://codedeep.cn:5502";
+const proBaseURL = "http://codedeep.cn:5502";
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL: proBaseURL;
 
 export const TIMEOUT = 10000;
